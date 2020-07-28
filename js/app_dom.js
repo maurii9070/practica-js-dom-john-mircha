a2b77d7f397e1d6682adfc857db0b9294a1c3688
Mauricio Velazquez <mauri_i9070@hotmail.com>
2020-07-27 22:09:34 -0300

    Se agrego un formulario con validaciones presionando las teclas(keyup)

--- js/app_dom.js
+++ js/app_dom.js
@@ -1,11 +1,13 @@
 import hamburgerMenu from './menu_hamburgesa.js';
 import { digitalClock } from './reloj.js';
+import contactFormValidations from './validaciones_formulario.js';
 
 const d = document;
 
 d.addEventListener('DOMContentLoaded', () => {
      hamburgerMenu('.panel-btn', '.panel', '.menu a');
      digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
+     contactFormValidations();
 });
 
 
